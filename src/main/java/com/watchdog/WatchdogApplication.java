9ad69efffc674bd6e12ce215671abeeae1b051d4
lsmin3388 <lsmin3388_@naver.com>
2024-12-24 14:57:35 +0900

    init: Add development environment setup

---
+++ src/main/java/com/watchdog/WatchdogApplication.java
@@ -0,0 +1,13 @@
+package com.watchdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchdogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchdogApplication.class, args);
+	}
+
+}
